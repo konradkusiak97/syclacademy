5040d2b843310cbc1fda892b4cc4fe2eea971652
konrad <konrad.kusiak@codeplay.com>
2023-06-16 14:15:02 +0100

    Excersize 7

--- Code_Exercises/Exercise_07_USM_Selector/source.cpp
+++ Code_Exercises/Exercise_07_USM_Selector/source.cpp
@@ -11,11 +11,26 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
+#include <sycl/sycl.hpp>
+
+int usm_selector(const sycl::device& dev) {
+  return dev.has(sycl::aspect::usm_device_allocations);
+}
+
 TEST_CASE("usm_selector", "usm_selector_source") {
 
   // Task: create a queue to a device which supports USM allocations
   // Remember to check for exceptions
-  auto usmQueue  = sycl::queue{};
+
+  try {
+    auto usmQueue  = sycl::queue{usm_selector};
+
+    std::cout << "Chosen device: " << usmQueue.get_device().get_info<sycl::info::device::name>() << std::endl;
+
+    usmQueue.throw_asynchronous();
+  } catch (const sycl::exception& e) {
+    std::cout << "Exception caught: " << e.what() << std::endl;
+  }
 
   REQUIRE(true);
 }
