95cc5193849cde26a73439bfa18dbb27e2da4805
Konrad <konrad.kusiak@codeplay.com>
2023-06-18 22:51:12 +0200

    Excersize 8

--- Code_Exercises/Exercise_08_USM_Vector_Add/source.cpp
+++ Code_Exercises/Exercise_08_USM_Vector_Add/source.cpp
@@ -11,6 +11,15 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
+#include <sycl/sycl.hpp>
+
+class usm_add;
+
+// USM selector
+int usm_selector(const sycl::device& dev) {
+  return dev.has(sycl::aspect::usm_device_allocations);
+}
+
 TEST_CASE("usm_vector_add", "usm_vector_add_source") {
   constexpr size_t dataSize = 1024;
 
@@ -21,11 +30,32 @@ TEST_CASE("usm_vector_add", "usm_vector_add_source") {
     r[i] = 0.0f;
   }
 
-  // Task: Allocate the arrays in USM, and compute r[i] = a[i] + b[i] on the SYCL device
-  for (int i = 0; i < dataSize; ++i) {
-    r[i] = a[i] + b[i];
-  }
+  try {
 
+    // Task: Allocate the arrays in USM, and compute r[i] = a[i] + b[i] on the SYCL device
+    auto queue = sycl::queue{usm_selector}; 
+
+    auto* device_ptrA = sycl::malloc_device<float>(dataSize, queue); 
+    auto* device_ptrB = sycl::malloc_device<float>(dataSize, queue); 
+    auto* device_ptrR = sycl::malloc_device<float>(dataSize, queue); 
+
+    queue.memcpy(device_ptrA, a, dataSize * sizeof(float)).wait();
+    queue.memcpy(device_ptrB, b, dataSize * sizeof(float)).wait();
+    queue.memcpy(device_ptrR, r, dataSize * sizeof(float)).wait();
+
+    queue.parallel_for<usm_add>(sycl::range(dataSize), [=](sycl::id<1> idx) {
+      device_ptrR[idx] = device_ptrA[idx] + device_ptrB[idx];
+
+    }).wait();
+
+    queue.memcpy(a, device_ptrA, dataSize * sizeof(float)).wait();
+    queue.memcpy(b, device_ptrB, dataSize * sizeof(float)).wait();
+    queue.memcpy(r, device_ptrR, dataSize * sizeof(float)).wait();
+
+    queue.throw_asynchronous();
+  } catch(const sycl::exception &e) {
+    std::cout << "Exception caught: " << e.what() << std::endl;
+  }
 
   for (int i = 0; i < dataSize; ++i) {
     REQUIRE(r[i] == i * 2);
