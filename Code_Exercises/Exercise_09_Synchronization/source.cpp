1e4987db08dde18f6660da4160a00bb3d126b07c
Konrad <konrad.kusiak@codeplay.com>
2023-06-19 09:17:45 +0200

    Excersize 9

--- Code_Exercises/Exercise_09_Synchronization/source.cpp
+++ Code_Exercises/Exercise_09_Synchronization/source.cpp
@@ -46,13 +46,114 @@
 
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
+#include <sycl/sycl.hpp>
+
+class usm_add;
+class vector_add;
+
+// USM selector
+int usm_selector(const sycl::device& dev) {
+  return dev.has(sycl::aspect::usm_device_allocations);
+}
 
 TEST_CASE("synchronization_usm", "synchronization_source") {
   // Use your code from Exercise 3 to start
+  constexpr size_t dataSize = 1024;
+
+  float a[dataSize], b[dataSize], r[dataSize];
+  for (int i = 0; i < dataSize; ++i) {
+    a[i] = static_cast<float>(i);
+    b[i] = static_cast<float>(i);
+    r[i] = 0.0f;
+  }
+
+  try {
+    auto queue = sycl::queue{usm_selector}; 
+
+    auto* device_ptrA = sycl::malloc_device<float>(dataSize, queue); 
+    auto* device_ptrB = sycl::malloc_device<float>(dataSize, queue); 
+    auto* device_ptrR = sycl::malloc_device<float>(dataSize, queue); 
+
+    queue.memcpy(device_ptrA, a, dataSize * sizeof(float)).wait();
+    queue.memcpy(device_ptrB, b, dataSize * sizeof(float)).wait();
+    queue.memcpy(device_ptrR, r, dataSize * sizeof(float)).wait();
+
+    queue.parallel_for<usm_add>(sycl::range(dataSize), [=](sycl::id<1> idx) {
+      device_ptrR[idx] = device_ptrA[idx] + device_ptrB[idx];
+
+    }).wait();
+
+    queue.memcpy(a, device_ptrA, dataSize * sizeof(float));
+    queue.memcpy(b, device_ptrB, dataSize * sizeof(float));
+    queue.memcpy(r, device_ptrR, dataSize * sizeof(float));
+
+    queue.wait_and_throw();
+
+    sycl::free(device_ptrA, queue);
+    sycl::free(device_ptrB, queue);
+    sycl::free(device_ptrR, queue);
+
+  } catch(const sycl::exception &e) {
+    std::cout << "Exception caught: " << e.what() << std::endl;
+  }
+
+  for (int i = 0; i < dataSize; ++i) {
+    REQUIRE(r[i] == i * 2);
+  }
   REQUIRE(true);
 }
 
 TEST_CASE("synchronization_buffer_acc", "synchronization_source") {
   // Use your code from Exercise 3 to start
+  constexpr size_t dataSize = 1024;
+
+  float a[dataSize], b[dataSize], r[dataSize];
+  for (int i = 0; i < dataSize; ++i) {
+    a[i] = static_cast<float>(i);
+    b[i] = static_cast<float>(i);
+    r[i] = 0.0f;
+  }
+
+  try {
+
+    auto queue = sycl::queue{sycl::gpu_selector_v};
+    std::cout << "Chosen device: "
+              << queue.get_device().get_info<sycl::info::device::name>()
+              << std::endl;
+
+    {
+      auto bufA = sycl::buffer{a, sycl::range{dataSize}};
+      auto bufB = sycl::buffer{b, sycl::range{dataSize}};
+      auto bufR = sycl::buffer{r, sycl::range{dataSize}};
+
+      queue.submit([&](sycl::handler &cgh) {
+        auto accA = sycl::accessor{bufA, cgh, sycl::read_only};
+        auto accB = sycl::accessor{bufB, cgh, sycl::read_only};
+        auto accR = sycl::accessor{bufR, cgh, sycl::write_only};
+
+        cgh.parallel_for<vector_add>(sycl::range<1>(dataSize), [=] (sycl::id<1> idx) {
+
+            accR[idx] = accA[idx] + accB[idx];
+        });
+
+      }).wait();
+
+      {
+        auto hostAccR = bufR.get_host_access(sycl::read_only);
+        for (int i = 0; i < dataSize; ++i) {
+          REQUIRE(hostAccR[i] == i * 2);
+        }
+      }
+
+    } // Copy data back
+    
+    queue.throw_asynchronous();
+  } catch (const sycl::exception &e) {
+    std::cout << "Exception caught: " << e.what() << std::endl;
+  }
+
+  for (int i = 0; i < dataSize; ++i) {
+    REQUIRE(r[i] == static_cast<float>(i) * 2.0f);
+  }
   REQUIRE(true);
 }
