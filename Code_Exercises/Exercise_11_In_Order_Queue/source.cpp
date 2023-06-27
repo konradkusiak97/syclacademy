018ac7b68157fed3846651ffc19a97bcebb7ee92
konrad <konrad.kusiak@codeplay.com>
2023-06-27 08:49:37 +0200

    Exersize 11

--- Code_Exercises/Exercise_11_In_Order_Queue/source.cpp
+++ Code_Exercises/Exercise_11_In_Order_Queue/source.cpp
@@ -58,7 +58,79 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
-TEST_CASE("in_order_queue", "in_order_queue_source") {
-  // Use the Exercise 10 solution to start
-  REQUIRE(true);
+#include <sycl/sycl.hpp>
+
+class kernelA; 
+class kernelB; 
+class kernelC; 
+class kernelOut; 
+
+TEST_CASE("managing_dependencies", "managing_dependencies_source") {
+  constexpr size_t dataSize = 1024;
+
+  int inA[dataSize], inB[dataSize], inC[dataSize], out[dataSize];
+  for (int i = 0; i < dataSize; ++i) {
+    inA[i] = static_cast<float>(i);
+    inB[i] = static_cast<float>(i);
+    inC[i] = static_cast<float>(i);
+    out[i] = 0.0f;
+  }
+
+  try {
+
+    auto queue = sycl::queue{sycl::gpu_selector_v, sycl::property::queue::in_order{}};
+
+    auto buffA = sycl::buffer{inA, sycl::range{dataSize}};
+    auto buffB = sycl::buffer{inB, sycl::range{dataSize}};
+    auto buffC = sycl::buffer{inC, sycl::range{dataSize}};
+    auto buffOut = sycl::buffer{out, sycl::range{dataSize}};
+
+    queue.submit([&](sycl::handler& cgh){
+      sycl::accessor accA{buffA, cgh, sycl::read_write};
+
+      cgh.parallel_for<kernelA>(sycl::range{dataSize}, [=](sycl::id<1> idx) {
+        accA[idx] = accA[idx] * 2.0f;
+      });
+    });
+
+    queue.submit([&](sycl::handler& cgh){
+      sycl::accessor accA{buffA, cgh, sycl::read_only};
+      sycl::accessor accB{buffB, cgh, sycl::write_only};
+
+      cgh.parallel_for<kernelB>(sycl::range{dataSize}, [=](sycl::id<1> idx) {
+        accB[idx] += accA[idx];
+      });
+    });
+
+    queue.submit([&](sycl::handler& cgh){
+      sycl::accessor accA{buffA, cgh, sycl::read_only};
+      sycl::accessor accC{buffC, cgh, sycl::write_only};
+
+      cgh.parallel_for<kernelC>(sycl::range{dataSize}, [=](sycl::id<1> idx) {
+        accC[idx] -= accA[idx];
+      });
+    });
+
+    queue.submit([&](sycl::handler& cgh){
+      sycl::accessor accB{buffB, cgh, sycl::read_only};
+      sycl::accessor accC{buffC, cgh, sycl::read_only};
+      sycl::accessor accOut{buffOut, cgh, sycl::write_only};
+
+      cgh.parallel_for<kernelOut>(sycl::range{dataSize}, [=](sycl::id<1> idx) {
+        accOut[idx] = accB[idx] + accC[idx];
+      });
+    });
+
+    queue.wait_and_throw();
+
+  } catch (const sycl::exception& e) {
+    std::cout << "Exception caught: " << e.what() << std::endl;
+  }
+
+  // Task: Run these kernels on the SYCL device, respecting the dependencies
+  // as shown in the README
+
+  for (int i = 0; i < dataSize; ++i) {
+    REQUIRE(out[i] == i * 2.0f);
+  }
 }
