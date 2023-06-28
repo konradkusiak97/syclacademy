22bc7060d7d5fe19bde69906e7d6040566d696cc
konrad <konrad.kusiak@codeplay.com>
2023-06-28 09:14:50 +0200

    Exersize 13

--- Code_Exercises/Exercise_13_Load_Balancing/source.cpp
+++ Code_Exercises/Exercise_13_Load_Balancing/source.cpp
@@ -45,6 +45,18 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
+#include <sycl/sycl.hpp>
+
+class vector_add_first;
+class vector_add_second;
+
+std::vector<sycl::device> get_two_devices() {
+  auto devs = sycl::device::get_devices();
+
+  if (devs.size() == 1) { return {devs[0], devs[0]}; }
+  else { return {devs[1], devs[2]}; }; 
+}
+
 TEST_CASE("load_balancing", "load_balancing_source") {
   constexpr size_t dataSize = 1024;
   constexpr float ratio = 0.5f;
@@ -58,18 +70,55 @@ TEST_CASE("load_balancing", "load_balancing_source") {
     r[i] = 0.0f;
   }
 
+  try {
+    std::vector<sycl::device> devices = get_two_devices();  
+
+    auto queueDev1 = sycl::queue{devices[0]};
+    auto queueDev2 = sycl::queue{devices[1]};
+
+    std::cout << "Running on devices:" << std::endl;
+    std::cout << "1: " << queueDev1.get_device().get_info<sycl::info::device::name>() << std::endl;
+    std::cout << "2: " << queueDev2.get_device().get_info<sycl::info::device::name>() << std::endl;
+
+    auto bufFirstA = sycl::buffer{a, sycl::range{dataSizeFirst}};
+    auto bufFirstB = sycl::buffer{b, sycl::range{dataSizeFirst}};
+    auto bufFirstR = sycl::buffer{r, sycl::range{dataSizeFirst}};
+
+    auto bufSecondA =
+        sycl::buffer{a + dataSizeFirst, sycl::range{dataSizeSecond}};
+    auto bufSecondB =
+        sycl::buffer{b + dataSizeFirst, sycl::range{dataSizeSecond}};
+    auto bufSecondR =
+        sycl::buffer{r + dataSizeFirst, sycl::range{dataSizeSecond}};
+
+    queueDev1.submit([&](sycl::handler &cgh) {
+      sycl::accessor accA{bufFirstA, cgh, sycl::read_only};
+      sycl::accessor accB{bufFirstB, cgh, sycl::read_only};
+      sycl::accessor accR{bufFirstR, cgh, sycl::write_only};
+
+      cgh.parallel_for<vector_add_first>(
+          sycl::range{dataSizeFirst},
+          [=](sycl::id<1> idx) { accR[idx] = accA[idx] + accB[idx]; });
+    });
+
+    queueDev2.submit([&](sycl::handler &cgh) {
+      sycl::accessor accA{bufSecondA, cgh, sycl::read_only};
+      sycl::accessor accB{bufSecondB, cgh, sycl::read_only};
+      sycl::accessor accR{bufSecondR, cgh, sycl::write_only};
+
+      cgh.parallel_for<vector_add_second>(
+          sycl::range{dataSizeSecond},
+          [=](sycl::id<1> idx) { accR[idx] = accA[idx] + accB[idx]; });
+    });
+
+    queueDev1.wait_and_throw();
+    queueDev2.wait_and_throw();
+
+  } catch (const sycl::exception& e) {
+    std::cout << "Exception caught: " << e.what() << std::endl;
+  }
   // Task: split the total work across two distinct SYCL devices
   // You might split the work as in the two loops below.
-
-  // Vector add for first part
-  for (int i = 0; i < dataSizeFirst; ++i) {
-    r[i] = a[i] + b[i];
-  }
-
-  // Vector add for second part
-  for (int i = 0; i < dataSizeSecond; ++i) {
-    r[dataSizeFirst + i] = a[dataSizeFirst + i] + b[dataSizeFirst + i];
-  }
 
   for (int i = 0; i < dataSize; ++i) {
     REQUIRE(r[i] == static_cast<float>(i) * 2.0f);
